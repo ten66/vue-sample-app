e1c039fc85288406b4ea18ad2604eb6eff1baa90
ten66 <ikesho.1217@gmail.com>
2021-12-03 11:54:08 +0900

    change many files

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Todo msg="todoリストを作っていきます" />
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import Todo from './components/Todo.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Todo
   }
 });
 </script>
